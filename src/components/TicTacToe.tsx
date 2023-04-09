dc887690d269a646387f89588c515a77c6ec7fe1
JosephGakah <joegakah@gmail.com>
2023-04-09 23:50:48 +0300

    Adding useEffects on state changes and usCallbacke

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMS, GAME_STATES } from "../constants";
 import { getRandomInt, switchPlayer } from "../utils/utils";
@@ -7,43 +7,64 @@ const emptyGrid = new Array(DIMENSIONS ** 2).fill(null);
  
 const TicTacToe = () => {
     const [grid, setGrid] = useState(emptyGrid)
-    const [gameState, setGameState] = useState('')
+    const [gameState, setGameState] = useState(GAME_STATES.notStarted)
     const [players, setPlayers] = useState<Record<string, number | null>>({
         human: null,
         ai: null
     })
 
+    const [nextMove, setNextMove] = useState<null|number>(null)
+
     const choosePlayer = (option: number) => {
         setPlayers({ human: option, ai: switchPlayer(option) });
         setGameState(GAME_STATES.inProgress);
+        setNextMove(PLAYER_X)
     };
  
-    const move = (index: number, player: number | null) => {
-        if(player !== null){
+    const move = useCallback(
+        (index: number, player: number | null) => {
+          if (player && gameState === GAME_STATES.inProgress) {
             setGrid((grid) => {
               const gridCopy = grid.concat();
               gridCopy[index] = player;
               return gridCopy;
             });
-        }
-    };
+          }
+        },
+        [gameState]
+      );
     
     const humanMove = (index: number) => {
         if (!grid[index]) {
             move(index, players.human);
-            aiMove();
+            setNextMove(players.ai);
         }
     };
 
-    const aiMove = () => {
+    const aiMove = useCallback(() => {
         let index = getRandomInt(0, 8);
         while (grid[index]) {
-            index = getRandomInt(0, 8);
+          index = getRandomInt(0, 8);
         }
+       
         move(index, players.ai);
-    };
+        setNextMove(players.human);
+       
+    }, [move, grid, players]);
 
-    return gameState == GAME_STATES.notStarted ? (
+    useEffect(() => {
+        let timeout: NodeJS.Timeout;
+        if ( nextMove !== null && nextMove === players.ai && gameState !== GAME_STATES.over ) {
+          // Delay AI moves to make them seem more natural
+          timeout = setTimeout(() => {
+            aiMove();
+          }, 500);
+        }
+        return () => timeout && clearTimeout(timeout);
+    }, [nextMove, aiMove, players.ai, gameState]);
+    
+
+    return gameState === GAME_STATES.notStarted ? (
         <div>
           <Inner>
             <p>Choose your player</p>
@@ -59,13 +80,13 @@ const TicTacToe = () => {
     : (
         <Container dims={DIMENSIONS}>
             {grid.map((value, index) => {
-            const isActive = value !== null;
-    
-            return (
-                <Square key={index} onClick={() => humanMove(index)}>
-                {isActive && <Marker>{value === PLAYER_X ? "X" : "O"}</Marker>}
-                </Square>
-            );
+                const isActive = value !== null;
+
+                return (
+                    <Square key={index} onClick={() => humanMove(index)}>
+                    {isActive && <Marker>{value === PLAYER_X ? "X" : "O"}</Marker>}
+                    </Square>
+                );
             })}
         </Container>
     )
